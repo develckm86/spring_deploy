d76eb7365fa2b6e5129005b87b57b05f838b1d6f
develckm86 <develckm86@gmail.com>
2025-04-22 11:39:22 +0900

    env.properties로 변경

--- src/main/java/com/tj703/l08_spring_jpa_rest/L08SpringJpaRestApplication.java
+++ src/main/java/com/tj703/l08_spring_jpa_rest/L08SpringJpaRestApplication.java
@@ -2,8 +2,11 @@ package com.tj703.l08_spring_jpa_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources(@PropertySource("classpath:/env.properties"))
 public class L08SpringJpaRestApplication {
 
     public static void main(String[] args) {
